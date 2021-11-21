c9b21826ed83fa5d0b9248a4af0a89bb8e27a73b
Sean Martin <sean@seanmartinstudio.com>
2021-11-21 11:14:59 -0800

    commit tree 9d38d171137559e02ede9c348a023b51f2e9d60d

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+
+
+function superbowlWin(record) {
+let winningYear = record.find(element => element.result === 'W');
+console.log(winningYear)
+    if (winningYear) {
+        return winningYear.year
+    } else {
+        return undefined
+    }
+}
